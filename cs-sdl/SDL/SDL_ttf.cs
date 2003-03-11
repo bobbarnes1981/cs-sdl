c82465e52e50f408bc628db6cb12d388565f68c4
cube77 <cube77@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2003-03-11 12:30:49 +0000

    Working version.

--- cs-sdl/SDL/SDL_ttf.cs
+++ cs-sdl/SDL/SDL_ttf.cs
@@ -74,8 +74,8 @@ namespace CsGL.SDL
 			it should NOT be used to fill a version structure, instead you should
 			use the TTF_VERSION() macro.
 		*/
-		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_version * TTF_Linked_Version();
+		//[DllImport("SDL_ttf.dll")]
+		//public static extern SDL_version * TTF_Linked_Version();
 
 		/* ZERO WIDTH NO-BREAKSPACE (Unicode byte order mark) */
 		//#define UNICODE_BOM_NATIVE	0xFEFF
@@ -100,80 +100,84 @@ namespace CsGL.SDL
 			* point size becomes the index of choosing which size.  If the value
 			* is too high, the last indexed size will be the default. */
 		[DllImport("SDL_ttf.dll")]
-		public static extern TTF_Font * TTF_OpenFont(char *file, int ptsize);
+		public static extern IntPtr TTF_OpenFont(string file, int ptsize);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern TTF_Font * TTF_OpenFontIndex(char *file, int ptsize, long index);
+		public static extern IntPtr TTF_OpenFontIndex(string file, int ptsize, long index);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern TTF_Font * TTF_OpenFontRW(SDL_RWops *src, int freesrc, int ptsize);
+		public static extern IntPtr TTF_OpenFontRW(SDL_RWops *src, int freesrc, int ptsize);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern TTF_Font * TTF_OpenFontIndexRW(SDL_RWops *src, int freesrc, int ptsize, long index);
+		public static extern IntPtr TTF_OpenFontIndexRW(SDL_RWops *src, int freesrc, int ptsize, long index);
 
 		/* Set and retrieve the font style
 				This font style is implemented by modifying the font glyphs, and
 				doesn't reflect any inherent properties of the truetype font file.
 		*/
-		//#define TTF_STYLE_NORMAL	0x00
-		//#define TTF_STYLE_BOLD		0x01
-		//#define TTF_STYLE_ITALIC	0x02
-		//#define TTF_STYLE_UNDERLINE	0x04
-		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_GetFontStyle(TTF_Font *font);
+		public enum TTF_STYLE : int
+		{
+			NORMAL = 0x00,
+			BOLD = 0x01,
+			ITALIC = 0x02,
+			UNDERLINE = 0x04
+		}
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern void TTF_SetFontStyle(TTF_Font *font, int style);
+		public static extern TTF_STYLE TTF_GetFontStyle(IntPtr font);
+
+		[DllImport("SDL_ttf.dll")]
+		public static extern void TTF_SetFontStyle(IntPtr font, TTF_STYLE style);
 
 		/* Get the total height of the font - usually equal to point size */
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_FontHeight(TTF_Font *font);
+		public static extern int TTF_FontHeight(IntPtr font);
 
 		/* Get the offset from the baseline to the top of the font
 				This is a positive value, relative to the baseline.
 			*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_FontAscent(TTF_Font *font);
+		public static extern int TTF_FontAscent(IntPtr font);
 
 		/* Get the offset from the baseline to the bottom of the font
 				This is a negative value, relative to the baseline.
 			*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_FontDescent(TTF_Font *font);
+		public static extern int TTF_FontDescent(IntPtr font);
 
 		/* Get the recommended spacing between lines of text for this font */
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_FontLineSkip(TTF_Font *font);
+		public static extern int TTF_FontLineSkip(IntPtr font);
 
 		/* Get the number of faces of the font */
 		[DllImport("SDL_ttf.dll")]
-		public static extern long TTF_FontFaces(TTF_Font *font);
+		public static extern long TTF_FontFaces(IntPtr font);
 
 		/* Get the font face attributes, if any */
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_FontFaceIsFixedWidth(TTF_Font *font);
+		public static extern int TTF_FontFaceIsFixedWidth(IntPtr font);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern char * TTF_FontFaceFamilyName(TTF_Font *font);
+		public static extern string TTF_FontFaceFamilyName(IntPtr font);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern char * TTF_FontFaceStyleName(TTF_Font *font);
+		public static extern string TTF_FontFaceStyleName(IntPtr font);
 
 		/* Get the metrics (dimensions) of a glyph */
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_GlyphMetrics(TTF_Font *font, Uint16 ch,
-																	int *minx, int *maxx,
-																	int *miny, int *maxy, int *advance);
+		public static extern int TTF_GlyphMetrics(IntPtr font, ushort ch,
+																	ref int minx, ref int maxx,
+																	ref int miny, ref int maxy, ref int advance);
 
 		/* Get the dimensions of a rendered string of text */
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_SizeText(TTF_Font *font, char *text, int *w, int *h);
+		public static extern int TTF_SizeText(IntPtr font, string text, ref int w, ref int h);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_SizeUTF8(TTF_Font *font, char *text, int *w, int *h);
+		public static extern int TTF_SizeUTF8(IntPtr font, string text, ref int w, ref int h);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern int TTF_SizeUNICODE(TTF_Font *font, Uint16 *text, int *w, int *h);
+		public static extern int TTF_SizeUNICODE(IntPtr font, string text, ref int w, ref int h);
 
 		/* Create an 8-bit palettized surface and render the given text at
 				fast quality with the given font and color.  The 0 pixel is the
@@ -182,16 +186,13 @@ namespace CsGL.SDL
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderText_Solid(TTF_Font *font,
-																		char *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderText_Solid(IntPtr font, string text, SDL_Color fg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUTF8_Solid(TTF_Font *font,
-																		char *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderUTF8_Solid(IntPtr font, string text, SDL_Color fg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUNICODE_Solid(TTF_Font *font,
-																		Uint16 *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderUNICODE_Solid(IntPtr font, string text, SDL_Color fg);
 
 		/* Create an 8-bit palettized surface and render the given glyph at
 				fast quality with the given font and color.  The 0 pixel is the
@@ -201,8 +202,7 @@ namespace CsGL.SDL
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderGlyph_Solid(TTF_Font *font,
-																		Uint16 ch, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderGlyph_Solid(IntPtr font, ushort ch, SDL_Color fg);
 
 		/* Create an 8-bit palettized surface and render the given text at
 				high quality with the given font and colors.  The 0 pixel is background,
@@ -210,16 +210,13 @@ namespace CsGL.SDL
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderText_Shaded(TTF_Font *font,
-																		char *text, SDL_Color fg, SDL_Color bg);
+		public static extern SDL_Surface * TTF_RenderText_Shaded(IntPtr font, string text, SDL_Color fg, SDL_Color bg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUTF8_Shaded(TTF_Font *font,
-																		char *text, SDL_Color fg, SDL_Color bg);
+		public static extern SDL_Surface * TTF_RenderUTF8_Shaded(IntPtr font, string text, SDL_Color fg, SDL_Color bg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUNICODE_Shaded(TTF_Font *font,
-																		Uint16 *text, SDL_Color fg, SDL_Color bg);
+		public static extern SDL_Surface * TTF_RenderUNICODE_Shaded(IntPtr font, string text, SDL_Color fg, SDL_Color bg);
 
 		/* Create an 8-bit palettized surface and render the given glyph at
 				high quality with the given font and colors.  The 0 pixel is background,
@@ -229,24 +226,20 @@ namespace CsGL.SDL
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderGlyph_Shaded(TTF_Font *font,
-																		Uint16 ch, SDL_Color fg, SDL_Color bg);
+		public static extern SDL_Surface * TTF_RenderGlyph_Shaded(IntPtr font, ushort ch, SDL_Color fg, SDL_Color bg);
 
 		/* Create a 32-bit ARGB surface and render the given text at high quality,
 				using alpha blending to dither the font with the given color.
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderText_Blended(TTF_Font *font,
-																		char *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderText_Blended(IntPtr font, string text, SDL_Color fg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUTF8_Blended(TTF_Font *font,
-																		char *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderUTF8_Blended(IntPtr font, string text, SDL_Color fg);
 
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderUNICODE_Blended(TTF_Font *font,
-																		Uint16 *text, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderUNICODE_Blended(IntPtr font, string text, SDL_Color fg);
 
 		/* Create a 32-bit ARGB surface and render the given glyph at high quality,
 				using alpha blending to dither the font with the given color.
@@ -255,28 +248,27 @@ namespace CsGL.SDL
 				This function returns the new surface, or NULL if there was an error.
 		*/
 		[DllImport("SDL_ttf.dll")]
-		public static extern SDL_Surface * TTF_RenderGlyph_Blended(TTF_Font *font,
-																		Uint16 ch, SDL_Color fg);
+		public static extern SDL_Surface * TTF_RenderGlyph_Blended(IntPtr font, ushort ch, SDL_Color fg);
 
 		/* For compatibility with previous versions, here are the old functions */
-		public static extern SDL_Surface * TTF_RenderText(TTF_Font *font, char *text, SDL_Color fg, SDL_Color bg)
+		public static SDL_Surface * TTF_RenderText(IntPtr font, string text, SDL_Color fg, SDL_Color bg)
 		{
 			return TTF_RenderText_Shaded(font, text, fg, bg);
 		}
 
-		public static extern SDL_Surface * TTF_RenderUTF8(TTF_Font *font, char *text, SDL_Color fg, SDL_Color bg)
+		public static SDL_Surface * TTF_RenderUTF8(IntPtr font, string text, SDL_Color fg, SDL_Color bg)
 		{
 			return TTF_RenderUTF8_Shaded(font, text, fg, bg);
 		}
 
-		public static extern SDL_Surface * TTF_RenderUNICODE(TTF_Font *font, Uint16 *text, SDL_Color fg, SDL_Color bg)
+		public static SDL_Surface * TTF_RenderUNICODE(IntPtr font, string text, SDL_Color fg, SDL_Color bg)
 		{
 			return TTF_RenderUNICODE_Shaded(font, text, fg, bg);
 		}
 
 		/* Close an opened font file */
 		[DllImport("SDL_ttf.dll")]
-		public static extern void TTF_CloseFont(TTF_Font *font);
+		public static extern void TTF_CloseFont(IntPtr font);
 
 		/* De-initialize the TTF engine */
 		[DllImport("SDL_ttf.dll")]
