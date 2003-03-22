8905ade730caf846bddea87bbacee8867de9ce09
ross_uk <ross_uk@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2003-03-22 20:04:03 +0000

    Changed ctor and factory to public for ttf

--- trunk/SDLDotNet/SDLException.cs
+++ trunk/SDLDotNet/SDLException.cs
@@ -7,16 +7,19 @@ namespace SDLDotNet {
 	/// <type>class</type>
 	/// <base>System.Exception</base>
 	public class SDLException : Exception {
-		internal SDLException(string msg) : base(msg) {}
 
-		internal static SDLException Generate() {
+		public SDLException(string msg) : base(msg) {}
+
+		public static SDLException Generate() {
 			string msg = Natives.SDL_GetError();
+
 			if (msg.IndexOf("Surface was lost") == -1)
 				return new SDLException(msg);
 			else
 				return new SurfaceLostException(msg);
 		}
 	}
+
 	/// <summary>
 	/// Represents an error resulting from a surface being lost, usually as a result of
 	/// the user changing the input focus away from a full-screen application
