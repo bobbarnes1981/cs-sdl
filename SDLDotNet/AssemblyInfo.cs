3c0b9cf61a573b804b51d6c19f127ae6231a6994
wweisser <wweisser@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2003-06-04 00:54:16 +0000

    updated version number, fixed key path, fixed documentation project

--- SDLDotNet/AssemblyInfo.cs
+++ SDLDotNet/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.4.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -54,5 +54,5 @@ using System.Runtime.CompilerServices;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("sdl.keys")]
+[assembly: AssemblyKeyFile("..\\..\\sdl.keys")]
 [assembly: AssemblyKeyName("")]
