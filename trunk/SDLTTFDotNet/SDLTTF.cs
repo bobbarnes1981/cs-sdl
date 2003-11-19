5f1c0108e55dabe3a511d04e65e64fec115ff7f8
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2003-11-19 18:00:53 +0000

    Put LGPL notice in files

--- trunk/SDLTTFDotNet/SDLTTF.cs
+++ trunk/SDLTTFDotNet/SDLTTF.cs
@@ -1,20 +1,43 @@
+/*
+ * $RCSfile$
+ * Copyright (C) 2003 Lucas Maloney
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * $Revision$
+ * $Date$
+ *
+ *	REVISION HISTORY
+ *
+ *	Mon 31 Mar 2003 23:28:02 EST LM
+ *	Changed namespace from SDLTTFDotNet
+ *	Now using singleton architecture
+ *
+ *	Tue 25 Mar 2003 17:50:09 EST LM
+ *	Added error check to TTF_Init call.  It will return -1 if there was a problem.
+ *
+ *	Mon 24 Mar 2003 20:45:40 EST LM
+ *	There is currently a bug in mono which meant this class did not need an instance of SDL.
+ *	I have fixed this so it does not depend on that bug.
+ */
+
 using System;
 using System.Runtime.InteropServices;
 using SDLDotNet;
-/*
-	REVISION HISTORY
-
-	Mon 31 Mar 2003 23:28:02 EST LM
-	Changed namespace from SDLTTFDotNet
-	Now using singleton architecture
-
-	Tue 25 Mar 2003 17:50:09 EST LM
-	Added error check to TTF_Init call.  It will return -1 if there was a problem.
-
-	Mon 24 Mar 2003 20:45:40 EST LM
-	There is currently a bug in mono which meant this class did not need an instance of SDL.
-	I have fixed this so it does not depend on that bug.
-*/
 //[assembly: CLSCompliantAttribute(true)]
 namespace SDLDotNet.TTF
 {
