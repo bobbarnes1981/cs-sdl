04c9f92f77ff0f2ec2d8b8707ab99621e494a037
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2005-01-13 22:11:16 +0000

    Start to integrate Smpeg

---
+++ SDLDotNet/src/Movie.cs
@@ -0,0 +1,355 @@
+/*
+ * $RCSfile$
+ * Copyright (C) 2004 David Hudson (jendave@yahoo.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+using System;
+using Tao.Sdl;
+
+namespace SdlDotNet 
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public enum MovieStatus
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		Playing = Smpeg.SMPEG_PLAYING,
+		/// <summary>
+		/// 
+		/// </summary>
+		Stopped = Smpeg.SMPEG_STOPPED,
+		/// <summary>
+		/// 
+		/// </summary>
+		Error = Smpeg.SMPEG_ERROR
+	}
+	/// <summary>
+	/// Represents a movie mpg file.
+	/// </summary>
+	public class Movie : BaseSdlResource 
+	{
+		private IntPtr handle;
+		private Smpeg.SMPEG_Info movieInfo;
+		private bool disposed = false;
+
+		internal Movie(IntPtr handle) 
+		{
+			this.handle = handle;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="file"></param>
+		public Movie(string file)
+		{
+			this.handle = Smpeg.SMPEG_new(file, out movieInfo, (int)SdlFlag.TrueValue);
+			this.movieInfo = movieInfo;
+			EnableVideo();
+			EnableAudio();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public IntPtr GetHandle
+		{ 
+			get
+			{
+				GC.KeepAlive(this);
+				return handle; 
+			}
+		}
+
+		/// <summary>
+		/// Destroys the surface object and frees its memory
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				try
+				{
+					if (disposing)
+					{
+					}
+					CloseHandle(handle);
+					GC.KeepAlive(this);
+					this.disposed = true;
+				}
+				finally
+				{
+					base.Dispose(disposing);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Closes Music handle
+		/// </summary>
+		protected override void CloseHandle(IntPtr handleToClose) 
+		{
+			if (handleToClose != IntPtr.Zero)
+			{
+				Smpeg.SMPEG_delete(handleToClose);
+				GC.KeepAlive(this);
+				handleToClose = IntPtr.Zero;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void EnableVideo()
+		{
+			Smpeg.SMPEG_enablevideo(handle, (int)SdlFlag.TrueValue);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void DisableVideo()
+		{
+			Smpeg.SMPEG_enablevideo(handle, (int)SdlFlag.FalseValue);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void EnableAudio()
+		{
+			Smpeg.SMPEG_enableaudio(handle, (int)SdlFlag.TrueValue);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void DisableAudio()
+		{
+			Smpeg.SMPEG_enableaudio(handle, (int)SdlFlag.FalseValue);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="surface"></param>
+		public void Display(Surface surface)
+		{
+			Smpeg.SMPEG_setdisplay(handle, surface.SurfacePointer, IntPtr.Zero, null);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="volume"></param>
+		public void AdjustVolume(int volume)
+		{
+			Smpeg.SMPEG_setvolume(handle, volume);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Width
+		{
+			get
+			{
+				return movieInfo.width;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Height
+		{
+			get
+			{
+				return movieInfo.height;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public double TotalTime
+		{
+			get
+			{
+				return movieInfo.total_time;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Size
+		{
+			get
+			{
+				return movieInfo.total_size;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public double CurrentFps
+		{
+			get
+			{
+				return movieInfo.current_fps;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public double CurrentFrame
+		{
+			get
+			{
+				return movieInfo.current_frame;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public double CurrentTime
+		{
+			get
+			{
+				return movieInfo.current_time;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int CurrentAudioFrame
+		{
+			get
+			{
+				return movieInfo.audio_current_frame;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string AudioString
+		{
+			get
+			{
+				return movieInfo.audio_string;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool HasAudio
+		{
+			get
+			{
+				if (movieInfo.has_audio != (int)SdlFlag.FalseValue)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool IsPlaying
+		{
+			get
+			{
+				if (Smpeg.SMPEG_status(handle) == (int)MovieStatus.Playing)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool IsStopped
+		{
+			get
+			{
+				if (Smpeg.SMPEG_status(handle) == (int)MovieStatus.Stopped)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool HasVideo
+		{
+			get
+			{
+				if (movieInfo.has_video != (int)SdlFlag.FalseValue)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void Play()
+		{
+			Smpeg.SMPEG_play(handle);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void Stop()
+		{
+			Smpeg.SMPEG_stop(handle);
+		}
+
+	}
+}
