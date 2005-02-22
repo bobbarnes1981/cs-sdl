849c4dd1b1060b71f5fc9302f651390e379fba3e
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2005-02-22 19:55:18 +0000

    Add LGPL license to MfGames stuff

--- SDLDotNet/examples/MfGames.Sdl.Dice/DiceConstants.cs
+++ SDLDotNet/examples/MfGames.Sdl.Dice/DiceConstants.cs
@@ -1,4 +1,23 @@
 /*
+ * $RCSfile$
+ * Copyright (C) 2004 D. R. E. Moonfire (d.moonfire@mfgames.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * DiceConstants.cs
  * 
  * THIS FILE HAS BEEN GENERATED AUTOMATICALLY. DO NOT EDIT!
@@ -8,7 +27,8 @@
  * Copyright (c) 2004 Moonfire Games. All rights reserved.
  */
 
-namespace MfGames.Utility.Dice {
+namespace MfGames.Utility.Dice 
+{
 
     /**
      * <remarks>An enumeration with token and production node
