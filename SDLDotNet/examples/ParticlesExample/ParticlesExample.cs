d8d8b3ce44b85c9f9802b22897fa7665d4300b24
robloach <robloach@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2005-10-18 00:53:05 +0000

    Initial commit of ParticlesExample.

---
+++ SDLDotNet/examples/ParticlesExample/ParticlesExample.cs
@@ -0,0 +1,72 @@
+using System;
+using SdlDotNet;
+using SdlDotNet.Particles;
+using SdlDotNet.Sprites;
+using System.Drawing;
+
+namespace ParticlesExample
+{
+	/// <summary>
+	/// An example program using particles.
+	/// </summary>
+	public class ParticlesExample
+	{
+		// Make a new particle system with some gravity
+		ParticleSystem particles = new ParticleSystem(0.3f);
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ParticlesExample()
+		{
+			
+			// Make the first particle (a pixel)
+			Particle first = new ParticlePixel(Color.White, 100,100,new Vector(-1,-7),70);
+			particles.Add(first); // Add it to the system
+
+			// Make the second particle (an animated sprite)
+			Animation anim = new Animation(new SurfaceCollection("../../Data/marble1.png", new Size(50,50)),1);
+			AnimatedSprite marble = new AnimatedSprite(anim);
+			marble.Animate = true;
+			Particle second = new ParticleSprite(marble, 200, 200, new Vector(2,-10), 500);
+			particles.Add(second); // Add it to the system
+
+			// Setup SDL.NET!
+			Video.SetVideoModeWindow(400,300);
+			Events.Tick+=new TickEventHandler(Events_Tick);
+		}
+
+		/// <summary>
+		/// Run the application
+		/// </summary>
+		public void Run()
+		{
+			Events.Run();
+		}
+
+		/// <summary>
+		/// Main entery point
+		/// </summary>
+		public static void Main()
+		{
+			ParticlesExample p = new ParticlesExample();
+			p.Run();
+		}
+
+		/// <summary>
+		/// An update tick
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Events_Tick(object sender, TickEventArgs e)
+		{
+			// Update all particles
+			particles.Update();
+
+			// Draw scene
+			Video.Screen.Fill(Color.Black);
+			particles.Render(Video.Screen);
+			Video.Screen.Update();
+		}
+	}
+}
