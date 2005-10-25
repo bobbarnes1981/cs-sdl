e76af4188d34d8cc047825ebfbd0be0d734b5665
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2005-10-25 06:02:49 +0000

    Fxcop updates

--- trunk/SDLDotNet/extras/Gui/AssemblyInfo.cs
+++ trunk/SDLDotNet/extras/Gui/AssemblyInfo.cs
@@ -1,19 +1,43 @@
+/*
+ * $RCSfile$
+ * Copyright (C) 2005 Rob Loach (http://www.robloach.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
 
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SdlDotNet.Gui")]
+[assembly: AssemblyDescription("SDL.NET Gui")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The SDL.NET Project")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Rob Loach")]
+[assembly: AssemblyTrademark("http://cs-sdl.sourceforge.net")]
 [assembly: AssemblyCulture("")]		
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -54,5 +78,12 @@ using System.Runtime.CompilerServices;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.Execution)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.SkipVerification)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.UnmanagedCode)]
+#if NET
+[assembly: AssemblyKeyFile("..\\..\\..\\..\\src\\SdlDotNet.snk")]
+#else
+[assembly: AssemblyKeyFile(@"SdlDotNet.snk")]
+#endif
--- trunk/SDLDotNet/extras/Gui/Widgets/WidgetCollection.cs
+++ trunk/SDLDotNet/extras/Gui/Widgets/WidgetCollection.cs
@@ -6,7 +6,7 @@ namespace SdlDotNet.Gui.Widgets
 	/// <summary>
 	/// A collection of widgets.
 	/// </summary>
-	public class WidgetCollection : CollectionBase
+	public class WidgetCollection : CollectionBase, ICollection
 	{
 		/// <summary>
 		/// 
@@ -39,6 +39,10 @@ namespace SdlDotNet.Gui.Widgets
 		/// <param name="widgets"></param>
 		public void Add(WidgetCollection widgets)
 		{
+			if (widgets == null)
+			{
+				throw new ArgumentNullException("widgets");
+			}
 			foreach(Widget widget in widgets)
 			{
 				Add(widget);
@@ -76,6 +80,56 @@ namespace SdlDotNet.Gui.Widgets
 			{
 				List[index] = value;
 			}
+		}
+
+		/// <summary>
+		/// Provide the explicit interface member for ICollection.
+		/// </summary>
+		/// <param name="array">Array to copy collection to</param>
+		/// <param name="index">Index at which to insert the collection items</param>
+		void ICollection.CopyTo(Array array, int index)
+		{
+			this.List.CopyTo(array, index);
+		}
+
+		/// <summary>
+		/// Provide the explicit interface member for ICollection.
+		/// </summary>
+		/// <param name="array">Array to copy collection to</param>
+		/// <param name="index">Index at which to insert the collection items</param>
+		public virtual void CopyTo(Widget[] array, int index)
+		{
+			((ICollection)this).CopyTo(array, index);
+		}
+
+		/// <summary>
+		/// Insert a widget into the collection
+		/// </summary>
+		/// <param name="index">Index at which to insert the widget</param>
+		/// <param name="widget">Particle to insert</param>
+		public virtual void Insert(int index, Widget widget)
+		{
+			List.Insert(index, widget);
+		} 
+
+		/// <summary>
+		/// Gets the index of the given Widget in the collection.
+		/// </summary>
+		/// <param name="widget">The widget to search for.</param>
+		/// <returns>The index of the given widget.</returns>
+		public virtual int IndexOf(Widget widget)
+		{
+			return List.IndexOf(widget);
+		} 
+
+		/// <summary>
+		/// Checks if widget is in the container
+		/// </summary>
+		/// <param name="widget">Widget to query for</param>
+		/// <returns>True is the Widget is in the container.</returns>
+		public bool Contains(Widget widget)
+		{
+			return (List.Contains(widget));
 		}
 	}
 }
