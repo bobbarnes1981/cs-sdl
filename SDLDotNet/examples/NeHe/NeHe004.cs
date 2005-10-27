a859b424a527ff40d102af9fce0b047ba4133c8d
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2005-10-27 21:15:21 +0000

    Adding NeHe tutorials

---
+++ SDLDotNet/examples/NeHe/NeHe004.cs
@@ -0,0 +1,95 @@
+#region License
+/*
+MIT License
+Copyright ©2003-2005 Tao Framework Team
+http://www.taoframework.com
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion License
+
+using System;
+using System.Reflection;
+
+using SdlDotNet;
+using Tao.OpenGl;
+
+namespace SdlDotNet.Examples
+{
+	class NeHe004 : NeHeBase
+	{
+		// Angle For The Triangle ( NEW )
+		private float rtri;                                              
+		private float rquad;
+    
+		public override void DrawGLScene()
+		{
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);        
+			// Clear Screen And Depth Buffer
+			Gl.glLoadIdentity();                                                
+			// Reset The Current Modelview Matrix
+			Gl.glTranslatef(-1.5f, 0, -6);                                      
+			// Move Left 1.5 Units And Into The Screen 6.0
+			Gl.glRotatef(rtri, 0, 1, 0);                                        
+			// Rotate The Triangle On The Y axis ( NEW )
+			Gl.glBegin(Gl.GL_TRIANGLES);                                        
+			// Drawing Using Triangles
+			Gl.glColor3f(1, 0, 0);                                          
+			// Set The Color To Red
+			Gl.glVertex3f(0, 1, 0);                                         
+			// Top
+			Gl.glColor3f(0, 1, 0);                                          
+			// Set The Color To Green
+			Gl.glVertex3f(-1, -1, 0);                                       
+			// Bottom Left
+			Gl.glColor3f(0, 0, 1);                                          
+			// Set The Color To Blue
+			Gl.glVertex3f(1, -1, 0);                                        
+			// Bottom Right
+			Gl.glEnd();                                                         
+			// Finished Drawing The Triangle
+			Gl.glLoadIdentity();                                                
+			// Reset The Current Modelview Matrix
+			Gl.glTranslatef(1.5f, 0, -6);                                       
+			// Move Right 1.5 Units And Into The Screen 6.0
+			Gl.glRotatef(rquad, 1, 0, 0);                                       
+			// Rotate The Quad On The X axis ( NEW )
+			Gl.glColor3f(0.5f, 0.5f, 1);                                        
+			// Set The Color To Blue One Time Only
+			Gl.glBegin(Gl.GL_QUADS);                                            
+			// Draw A Quad
+			Gl.glVertex3f(-1, 1, 0);                                        
+			// Top Left
+			Gl.glVertex3f(1, 1, 0);                                         
+			// Top Right
+			Gl.glVertex3f(1, -1, 0);                                        
+			// Bottom Right
+			Gl.glVertex3f(-1, -1, 0);                                       
+			// Bottom Left
+			Gl.glEnd();                                                         
+			// Done Drawing The Quad
+			rtri += 0.2f;                                                       
+			// Increase The Rotation Variable For The Triangle ( NEW )
+			rquad -= 0.15f;                                                     
+			// Decrease The Rotation Variable For The Quad ( NEW )
+			Video.GLSwapBuffers();
+		}
+	}
+}
---
+++ SDLDotNet/examples/NeHe/NeHe006.cs
@@ -0,0 +1,163 @@
+#region License
+/*
+MIT License
+Copyright ©2003-2005 Tao Framework Team
+http://www.taoframework.com
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion License
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+
+using SdlDotNet;
+using Tao.OpenGl;
+
+namespace SdlDotNet.Examples
+{
+	class NeHe006 : NeHeBase
+	{    
+		private float xrot;                                              
+		// X Rotation ( NEW )
+		private float yrot;                                              
+		// Y Rotation ( NEW )
+		private float zrot;                                              
+		// Z Rotation ( NEW )
+		private int[] texture = new int[1];                              
+		// Storage For One Texture ( NEW )
+		string data_directory = @"Data/";
+		string filepath = @"../../";
+
+		public override void InitGL()
+		{
+			this.LoadGLTextures();
+			Gl.glEnable(Gl.GL_TEXTURE_2D);                                      
+			// Enable Texture Mapping ( NEW )
+			base.InitGL ();
+		}
+
+		public override void DrawGLScene()
+		{
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);        
+			// Clear The Screen And The Depth Buffer
+			Gl.glLoadIdentity();                                                
+			// Reset The View
+			Gl.glTranslatef(0, 0, -5);
+
+			Gl.glRotatef(xrot, 1, 0, 0);
+			Gl.glRotatef(yrot, 0, 1, 0);
+			Gl.glRotatef(zrot, 0, 0, 1);
+
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture[0]);
+
+			Gl.glBegin(Gl.GL_QUADS);
+			// Front Face
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(-1, -1, 1);
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(1, -1, 1);
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(1, 1, 1);
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(-1, 1, 1);
+			// Back Face
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(-1, -1, -1);
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(-1, 1, -1);
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(1, 1, -1);
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(1, -1, -1);
+			// Top Face
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(-1, 1, -1);
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(-1, 1, 1);
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(1, 1, 1);
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(1, 1, -1);
+			// Bottom Face
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(-1, -1, -1);
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(1, -1, -1);
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(1, -1, 1);
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(-1, -1, 1);
+			// Right face
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(1, -1, -1);
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(1, 1, -1);
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(1, 1, 1);
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(1, -1, 1);
+			// Left Face
+			Gl.glTexCoord2f(0, 0); Gl.glVertex3f(-1, -1, -1);
+			Gl.glTexCoord2f(1, 0); Gl.glVertex3f(-1, -1, 1);
+			Gl.glTexCoord2f(1, 1); Gl.glVertex3f(-1, 1, 1);
+			Gl.glTexCoord2f(0, 1); Gl.glVertex3f(-1, 1, -1);
+			Gl.glEnd();
+
+			xrot += 0.3f;
+			yrot += 0.2f;
+			zrot += 0.4f;
+			Video.GLSwapBuffers();
+		}
+
+		#region void LoadGLTextures()
+		/// <summary>
+		///     Load bitmaps and convert to textures.
+		/// </summary>
+		protected virtual void LoadGLTextures() 
+		{
+			if (File.Exists(data_directory + "NeHe006.bmp"))
+			{
+				filepath = "";
+			}                                              
+			// Status Indicator
+			Bitmap[] textureImage = new Bitmap[1];                              
+			// Create Storage Space For The Texture
+
+			textureImage[0] = new Bitmap(filepath + data_directory + "NeHe006.bmp");                
+			// Load The Bitmap
+			// Check For Errors, If Bitmap's Not Found, Quit
+			if(textureImage[0] != null) 
+			{
+				Gl.glGenTextures(1, out texture[0]);                            
+				// Create The Texture
+
+				textureImage[0].RotateFlip(RotateFlipType.RotateNoneFlipY);     
+				// Flip The Bitmap Along The Y-Axis
+				// Rectangle For Locking The Bitmap In Memory
+				Rectangle rectangle = 
+					new Rectangle(0, 0, textureImage[0].Width, textureImage[0].Height);
+				// Get The Bitmap's Pixel Data From The Locked Bitmap
+				BitmapData bitmapData = 
+					textureImage[0].LockBits(rectangle, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+				// Typical Texture Generation Using Data From The Bitmap
+				Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture[0]);
+				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB8, textureImage[0].Width, textureImage[0].Height, 0, Gl.GL_BGR, Gl.GL_UNSIGNED_BYTE, bitmapData.Scan0);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+
+				if(textureImage[0] != null) 
+				{                                   
+					// If Texture Exists
+					textureImage[0].UnlockBits(bitmapData);                     
+					// Unlock The Pixel Data From Memory
+					textureImage[0].Dispose();                                  
+					// Dispose The Bitmap
+				}
+			}                                                     
+		}
+		#endregion bool LoadGLTextures()
+
+	}
+}
