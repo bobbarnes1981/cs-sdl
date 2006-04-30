f521abd91eb658215adc98663ca86e45c04d6e9e
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-04-30 00:18:28 +0000

    Major updates

--- SdlDotNet/extras/OpenGl/SurfaceGl.cs
+++ SdlDotNet/extras/OpenGl/SurfaceGl.cs
@@ -31,9 +31,8 @@ namespace SdlDotNet.OpenGl
 	public class SurfaceGl
 	{
 		Surface surface;
-		Surface tempSurface;
 		Bitmap textureImage;
-		int texture;
+		int textureID;
 
 		/// <summary>
 		/// 
@@ -41,17 +40,13 @@ namespace SdlDotNet.OpenGl
 		/// <param name="surface"></param>
 		public SurfaceGl(Surface surface)
 		{
-			this.surface = surface;
+			this.Surface = surface;
 		}
 
 		/// <summary>
 		/// 
 		/// </summary>
 		public SurfaceGl()
-		{
-		}
-
-		static SurfaceGl()
 		{
 		}
 
@@ -62,38 +57,41 @@ namespace SdlDotNet.OpenGl
 		{
 			get
 			{
-				return this.texture;
+				if (this.textureID == 0)
+				{
+					Gl.glGenTextures(1, out this.textureID);
+				}
+				return this.textureID;
 			}
 		}
 
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <returns></returns>
-		public int GenerateTexture()
+		public Bitmap TextureImage
 		{
-			this.tempSurface = new Surface(NextPowerOfTwo(surface.Width), NextPowerOfTwo(surface.Height));
-			this.tempSurface.Blit(surface);
-			// Status Indicator
-			this.textureImage = tempSurface.Bitmap;   
-			// Create Storage Space For The Texture	
-			// Load The Bitmap
-			// Check For Errors, If Bitmap's Not Found, Quit
-			if(textureImage != null) 
+			get
 			{
-				// Create The Texture
-				Gl.glGenTextures(1, out this.texture); 
-				return this.texture;
-				
+				return this.textureImage;
 			}
-			else
-			{
-				return 0;
-			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Surface ResizeSurface(Surface surface)
+		{
+			return surface.Resize(new Size(NextPowerOfTwo(this.surface.Width), NextPowerOfTwo(this.surface.Height)));
 		}
 
 		private static int NextPowerOfTwo(int x)
 		{
+//			int bit = 1;
+//			while ( bit < x)
+//			{
+//				bit <<=1;
+//			}
+//			return bit;
 			double logbase2 = (Math.Log(x) / Math.Log(2));
 			return (int)Math.Round(Math.Pow(2, Math.Ceiling(logbase2)));
 		}
@@ -117,8 +115,9 @@ namespace SdlDotNet.OpenGl
 			}
 			set
 			{
-				this.surface = value;
-				this.GenerateTexture();
+				this.surface = value.Resize(new Size(NextPowerOfTwo(value.Width), NextPowerOfTwo(value.Height)));;
+				this.textureID = this.TextureID;
+				this.textureImage = this.surface.Bitmap;
 			}
 		}
 
@@ -127,70 +126,87 @@ namespace SdlDotNet.OpenGl
 		/// </summary>
 		public void Draw(Point location) 
 		{
-			// Rectangle For Locking The Bitmap In Memory
-			Rectangle rectangle = 
-				new Rectangle(0, 0, textureImage.Width, textureImage.Height);
-			// Get The Bitmap's Pixel Data From The Locked Bitmap
-			BitmapData bitmapData = 
-				textureImage.LockBits(rectangle, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-						
-			// Typical Texture Generation Using Data From The Bitmap
-			Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture);
-			Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB8, textureImage.Width, textureImage.Height, 0, Gl.GL_BGR, Gl.GL_UNSIGNED_BYTE, bitmapData.Scan0);
-					
-			Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
-			Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
-
-			int[] vPort= new int[4];
-  
-			Gl.glGetIntegerv(Gl.GL_VIEWPORT, vPort);
-  
-			Gl.glMatrixMode(Gl.GL_PROJECTION);
-			Gl.glPushMatrix();
-			Gl.glLoadIdentity();
-  
-			Gl.glOrtho(0, vPort[2], 0, vPort[3], -1, 1);
-			Gl.glMatrixMode(Gl.GL_MODELVIEW);
-			Gl.glPushMatrix();
-			Gl.glLoadIdentity();
-
-			Gl.glDisable(Gl.GL_DEPTH_TEST);
-			Gl.glEnable(Gl.GL_TEXTURE_2D);
-			Gl.glColor3f(1.0f, 1.0f, 1.0f);
-	
-			/* Draw a quad at location */
-			Gl.glBegin(Gl.GL_QUADS);
-			/* Recall that the origin is in the lower-left corner
-				   That is why the TexCoords specify different corners
-				   than the Vertex coors seem to. */
-			Gl.glTexCoord2f(0.0f, 1.0f); 
-			Gl.glVertex2f(location.X, location.Y);
-			Gl.glTexCoord2f(1.0f, 1.0f); 
-			Gl.glVertex2f(location.X + textureImage.Width, location.Y);
-			Gl.glTexCoord2f(1.0f, 0.0f); 
-			Gl.glVertex2f(location.X + textureImage.Width, location.Y + textureImage.Height);
-			Gl.glTexCoord2f(0.0f, 0.0f); 
-			Gl.glVertex2f(location.X, location.Y+ textureImage.Height);
-			Gl.glEnd();
-	
-			/* Bad things happen if we delete the texture before it finishes */
-			Gl.glFinish();
-
-			Gl.glDeleteTextures(1, ref texture);
-
-			Gl.glEnable(Gl.GL_DEPTH_TEST);
-			Gl.glMatrixMode(Gl.GL_PROJECTION);
-			Gl.glPopMatrix();   
-			Gl.glMatrixMode(Gl.GL_MODELVIEW);
-			Gl.glPopMatrix();
-
-			if(textureImage != null) 
+			if (textureImage != null)
 			{
-				// If Texture Exists
-				textureImage.UnlockBits(bitmapData); 
-				// Unlock The Pixel Data From Memory
-				textureImage.Dispose();   
-				// Dispose The Bitmap
+				Surface screen = Video.Screen;
+
+				this.textureImage.RotateFlip(RotateFlipType.RotateNoneFlipY); 
+				/* Note, there may be other things you need to change,
+				   depending on how you have your OpenGL state set up.
+				*/
+				Gl.glPushAttrib(Gl.GL_ENABLE_BIT);
+				Gl.glDisable(Gl.GL_DEPTH_TEST);
+				Gl.glDisable(Gl.GL_CULL_FACE);
+				Gl.glEnable(Gl.GL_TEXTURE_2D);
+
+				/* This allows alpha blending of 2D textures with the scene */
+				Gl.glEnable(Gl.GL_BLEND);
+				Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+
+				Gl.glViewport(0, 0, screen.Width, screen.Height);
+
+				Gl.glMatrixMode(Gl.GL_PROJECTION);
+				Gl.glPushMatrix();
+				Gl.glLoadIdentity();
+
+				Gl.glOrtho(0.0, (double)screen.Width, (double)screen.Height, 0.0, 0.0, 1.0);
+
+				Gl.glMatrixMode(Gl.GL_MODELVIEW);
+				Gl.glPushMatrix();
+				Gl.glLoadIdentity();
+
+				Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_DECAL);
+
+				// Rectangle For Locking The Bitmap In Memory
+				Rectangle rectangle = 
+					new Rectangle(0, 0, textureImage.Width, textureImage.Height);
+
+				// Get The Bitmap's Pixel Data From The Locked Bitmap
+				BitmapData bitmapData = 
+					textureImage.LockBits(rectangle, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+						
+				// Typical Texture Generation Using Data From The Bitmap
+				Gl.glBindTexture(Gl.GL_TEXTURE_2D, this.textureID);
+				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, textureImage.Width, textureImage.Height, 0, Gl.GL_BGR, Gl.GL_UNSIGNED_BYTE, bitmapData.Scan0);
+					
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+	
+				/* Draw a quad at location */
+				Gl.glBegin(Gl.GL_QUADS);
+				/* Recall that the origin is in the lower-left corner
+					   That is why the TexCoords specify different corners
+					   than the Vertex coors seem to. */
+				Gl.glTexCoord2f(0.0f, 1.0f); 
+				Gl.glVertex2f(location.X, location.Y);
+				Gl.glTexCoord2f(1.0f, 1.0f); 
+				Gl.glVertex2f(location.X + textureImage.Width, location.Y);
+				Gl.glTexCoord2f(1.0f, 0.0f); 
+				Gl.glVertex2f(location.X + textureImage.Width, location.Y + textureImage.Height);
+				Gl.glTexCoord2f(0.0f, 0.0f); 
+				Gl.glVertex2f(location.X, location.Y+ textureImage.Height);
+				Gl.glEnd();
+	
+				/* Bad things happen if we delete the texture before it finishes */
+				Gl.glFinish();
+
+				Gl.glDeleteTextures(1, ref this.textureID);
+				Gl.glDisable(Gl.GL_BLEND);	
+				Gl.glEnable(Gl.GL_DEPTH_TEST);
+				Gl.glMatrixMode(Gl.GL_MODELVIEW);
+				Gl.glPopMatrix();
+				Gl.glMatrixMode(Gl.GL_PROJECTION);
+				Gl.glPopMatrix();
+				Gl.glPopAttrib();
+
+				if(textureImage != null) 
+				{
+					// If Texture Exists
+					textureImage.UnlockBits(bitmapData); 
+					// Unlock The Pixel Data From Memory
+					textureImage.Dispose();   
+					// Dispose The Bitmap
+				}
 			}
 		}
 	}
