b70fd5c17f1ac06f72dc8e8218c3ac0400eca3c3
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-05-06 06:03:36 +0000

    move old Tess files into Mezzanine directory

---
+++ trunk/Mezzanine/MezzanineLib/Render/RenderText.cs
@@ -0,0 +1,319 @@
+#region License
+/*
+ * Copyright (C) 2001-2005 Wouter van Oortmerssen.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ * 
+ * additional clause specific to Cube:
+ * 
+ * 4. Source versions may not be "relicensed" under a different license
+ * without my explicitly written permission.
+ *
+ */
+
+/* 
+ * All code Copyright (C) 2006 David Y. Hudson
+ */
+#endregion License
+// rendertext.cpp: based on Don's gl_text.cpp
+using System;
+using System.Text;
+using System.IO;
+using Tao.Sdl;
+using Tao.OpenGl;
+using SdlDotNet;
+using System.Runtime.InteropServices;
+
+namespace TessLib.Render
+{
+	public class RenderText
+	{
+		static short[,] char_coords = new short[,] 
+			{
+	{0,0,25,64},        //!
+	{25,0,54,64},       //"
+	{54,0,107,64},      //#
+	{107,0,148,64},     //$
+	{148,0,217,64},     //%
+	{217,0,263,64},     //&
+	{263,0,280,64},     //'
+	{280,0,309,64},     //(
+	{309,0,338,64},     //)
+	{338,0,379,64},     //*
+	{379,0,432,64},     //+
+	{432,0,455,64},     //,
+	{455,0,484,64},     //-
+	{0,64,21,128},      //.
+	{23,64,52,128},     ///
+	{52,64,93,128},     //0
+	{93,64,133,128},    //1
+	{133,64,174,128},   //2
+	{174,64,215,128},   //3
+	{215,64,256,128},   //4
+	{256,64,296,128},   //5
+	{296,64,337,128},   //6
+	{337,64,378,128},   //7
+	{378,64,419,128},   //8
+	{419,64,459,128},   //9
+	{459,64,488,128},   //:
+	{0,128,29,192},     //;
+	{29,128,81,192},    //<
+	{81,128,134,192},   //=
+	{134,128,186,192},  //>
+	{186,128,221,192},  //?
+	{221,128,285,192},  //@
+	{285,128,329,192},  //A
+	{329,128,373,192},  //B
+	{373,128,418,192},  //C
+	{418,128,467,192},  //D
+	{0,192,40,256},     //E
+	{40,192,77,256},    //F
+	{77,192,127,256},   //G
+	{127,192,175,256},  //H
+	{175,192,202,256},  //I
+	{202,192,231,256},  //J
+	{231,192,275,256},  //K
+	{275,192,311,256},  //L
+	{311,192,365,256},  //M
+	{365,192,413,256},  //N
+	{413,192,463,256},  //O
+	{1,256,38,320},     //P
+	{38,256,89,320},    //Q
+	{89,256,133,320},   //R
+	{133,256,176,320},  //S
+	{177,256,216,320},  //T
+	{217,256,263,320},  //U
+	{263,256,307,320},  //V
+	{307,256,370,320},  //W
+	{370,256,414,320},  //X
+	{414,256,453,320},  //Y
+	{453,256,497,320},  //Z
+	{0,320,29,384},     //[
+	{29,320,58,384},    //"\"
+	{59,320,87,384},    //]
+	{87,320,139,384},   //^
+	{139,320,180,384},  //_
+	{180,320,221,384},  //`
+	{221,320,259,384},  //a
+	{259,320,299,384},  //b
+	{299,320,332,384},  //c
+	{332,320,372,384},  //d
+	{372,320,411,384},  //e
+	{411,320,433,384},  //f
+	{435,320,473,384},  //g
+	{0,384,40,448},     //h
+	{40,384,56,448},    //i
+	{58,384,80,448},    //j
+	{80,384,118,448},   //k
+	{118,384,135,448},  //l
+	{135,384,197,448},  //m
+	{197,384,238,448},  //n
+	{238,384,277,448},  //o
+	{277,384,317,448},  //p
+	{317,384,356,448},  //q
+	{357,384,384,448},  //r
+	{385,384,417,448},  //s
+	{417,384,442,448},  //t
+	{443,384,483,448},  //u
+	{0,448,38,512},     //v
+	{38,448,90,512},    //w
+	{90,448,128,512},   //x
+	{128,448,166,512},  //y
+	{166,448,200,512},  //z
+	{200,448,241,512},  //{
+	{241,448,270,512},  //|
+	{270,448,310,512},  //}
+	{310,448,363,512}  //~
+			};
+		
+		static int PIXELTAB = 200;
+
+		public static int TextWidth(string str)
+		{
+			int x = 0;
+//			System.Text.ASCIIEncoding encode = new ASCIIEncoding();
+//			//encode.GetString(
+//			char[] c = new char[0];
+//			for (int i = 0; i<str.Length; i++)
+//			{
+//				
+//				c[0] = str[i]; //int c originally - why?
+//				if(c[0]=='\t') 
+//				{ 
+//					x = (x+PIXELTAB)/PIXELTAB*PIXELTAB;
+//					continue; 
+//				}; 
+//				if(c[0]=='\f')
+//				{
+//					continue; 
+//				}
+//				if(c[0]==' ') 
+//				{ 
+//					x += GameInit.FontH/2; 
+//					continue; 
+//				};
+//				int temp = encode.GetBytes(c);
+//				
+//				//c -= 33;
+//				if(c[0]<0 || c[0]>=95) continue;
+//				int in_width = char_coords[c, 2] - char_coords[c, 0];
+//				x += in_width + 1;
+//			}
+			return x;
+		}
+
+		/// <summary>
+		/// TODO: does not work
+		/// </summary>
+		/// <param name="fstr"></param>
+		/// <param name="left"></param>
+		/// <param name="top"></param>
+		/// <param name="gl_num"></param>
+		/// <param name="str"></param>
+		public static void DrawTextF(string fstr, int left, int top, int gl_num, string str)
+		{
+			//sprintf_sdlv(str, gl_num, fstr);
+			DrawText(str, left, top, gl_num);
+		}
+
+		public static void DrawText(string str, int left, int top, int gl_num)
+		{
+			Gl.glBlendFunc(Gl.GL_ONE, Gl.GL_ONE);
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, gl_num);
+			Gl.glColor3ub(255,255,255);
+			
+			int x = left;
+			int y = top;
+			
+			int i;
+			float in_left, in_top, in_right, in_bottom;
+			int in_width, in_height;
+			
+			//			for (i = 0; str[i] != 0; i++)
+			//			{
+			//				int c = str[i];
+			//				if(c=='\t') { x = (x-left+PIXELTAB)/PIXELTAB*PIXELTAB+left; continue; }; 
+			//				if(c=='\f') { Gl.glColor3ub(64,255,128); continue; };
+			//				if(c==' ') { x += GameInit.FontH/2; continue; };
+			//				c -= 33;
+			//				if(c<0 || c>=95) continue;
+			//
+			//				in_left    = ((float) char_coords[c][0])   / 512.0f;
+			//				in_top     = ((float) char_coords[c][1]+2) / 512.0f;
+			//				in_right   = ((float) char_coords[c][2])   / 512.0f;
+			//				in_bottom  = ((float) char_coords[c][3]-2) / 512.0f;
+			//
+			//				in_width   = char_coords[c][2] - char_coords[c][0];
+			//				in_height  = char_coords[c][3] - char_coords[c][1];
+			//
+			//				Gl.glBegin(Gl.GL_QUADS);
+			//				Gl.glTexCoord2f(in_left,  in_top   ); Gl.glVertex2i(x,            y);
+			//				Gl.glTexCoord2f(in_right, in_top   ); Gl.glVertex2i(x + in_width, y);
+			//				Gl.glTexCoord2f(in_right, in_bottom); Gl.glVertex2i(x + in_width, y + in_height);
+			//				Gl.glTexCoord2f(in_left,  in_bottom); Gl.glVertex2i(x,            y + in_height);
+			//				Gl.glEnd();
+			//        
+			//				RenderGl.XtraVerts += 4;
+			//				x += in_width  + 1;
+			//			}
+		}
+
+		// also Don's code, so goes in here too :)
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s0"></param>
+		/// <param name="t0"></param>
+		/// <param name="x0"></param>
+		/// <param name="y0"></param>
+		/// <param name="z0"></param>
+		/// <param name="s1"></param>
+		/// <param name="t1"></param>
+		/// <param name="x1"></param>
+		/// <param name="y1"></param>
+		/// <param name="z1"></param>
+		/// <param name="s2"></param>
+		/// <param name="t2"></param>
+		/// <param name="x2"></param>
+		/// <param name="y2"></param>
+		/// <param name="z2"></param>
+		/// <param name="s3"></param>
+		/// <param name="t3"></param>
+		/// <param name="x3"></param>
+		/// <param name="y3"></param>
+		/// <param name="z3"></param>
+		/// <param name="texture"></param>
+		public static void DrawEnvBoxAux(float s0, float t0, int x0, int y0, int z0,
+			float s1, float t1, int x1, int y1, int z1,
+			float s2, float t2, int x2, int y2, int z2,
+			float s3, float t3, int x3, int y3, int z3,
+			int texture)
+		{
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture);
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glTexCoord2f(s3, t3); Gl.glVertex3d(x3, y3, z3);
+			Gl.glTexCoord2f(s2, t2); Gl.glVertex3d(x2, y2, z2);
+			Gl.glTexCoord2f(s1, t1); Gl.glVertex3d(x1, y1, z1);
+			Gl.glTexCoord2f(s0, t0); Gl.glVertex3d(x0, y0, z0);
+			Gl.glEnd();
+			RenderGl.XtraVerts += 4;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="t"></param>
+		/// <param name="w"></param>
+		public static void DrawEnvBox(int t, int w)
+		{
+			Gl.glDepthMask(Gl.GL_FALSE);
+
+			DrawEnvBoxAux(1.0f, 1.0f, -w, -w,  w,
+				0.0f, 1.0f,  w, -w,  w,
+				0.0f, 0.0f,  w, -w, -w,
+				1.0f, 0.0f, -w, -w, -w, t);
+
+			DrawEnvBoxAux(1.0f, 1.0f, +w,  w,  w,
+				0.0f, 1.0f, -w,  w,  w,
+				0.0f, 0.0f, -w,  w, -w,
+				1.0f, 0.0f, +w,  w, -w, t+1);
+
+			DrawEnvBoxAux(0.0f, 0.0f, -w, -w, -w,
+				1.0f, 0.0f, -w,  w, -w,
+				1.0f, 1.0f, -w,  w,  w,
+				0.0f, 1.0f, -w, -w,  w, t+2);
+
+			DrawEnvBoxAux(1.0f, 1.0f, +w, -w,  w,
+				0.0f, 1.0f, +w,  w,  w,
+				0.0f, 0.0f, +w,  w, -w,
+				1.0f, 0.0f, +w, -w, -w, t+3);
+
+			DrawEnvBoxAux(0.0f, 1.0f, -w,  w,  w,
+				0.0f, 0.0f, +w,  w,  w,
+				1.0f, 0.0f, +w, -w,  w,
+				1.0f, 1.0f, -w, -w,  w, t+4);
+
+			DrawEnvBoxAux(0.0f, 1.0f, +w,  w, -w,
+				0.0f, 0.0f, -w,  w, -w,
+				1.0f, 0.0f, -w, -w, -w,
+				1.0f, 1.0f, +w, -w, -w, t+5);
+
+			Gl.glDepthMask(Gl.GL_TRUE);
+		}
+	}
+}
---
+++ trunk/Mezzanine/MezzanineManagedLib/src/protos.h
@@ -0,0 +1,256 @@
+// protos for ALL external functions in cube... 
+#ifndef DECLSPEC 
+define DECLSPEC	__declspec(dllexport)
+#endif
+#ifndef CDECL
+#define CDECL __cdecl
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// command
+extern DECLSPEC int CDECL variable(char *name, int min, int cur, int max, int *storage, void (*fun)(), bool persist);
+extern DECLSPEC void CDECL setvar(char *name, int i);
+extern DECLSPEC int CDECL getvar(char *name);
+extern DECLSPEC bool CDECL identexists(char *name);
+extern DECLSPEC bool CDECL addcommand(char *name, void (*fun)(), int narg);
+extern DECLSPEC int CDECL execute(char *p, bool down = true);
+extern DECLSPEC void CDECL exec(char *cfgfile);
+extern DECLSPEC bool CDECL execfile(char *cfgfile);
+extern DECLSPEC void CDECL resetcomplete();
+extern DECLSPEC void CDECL complete(char *s);
+extern DECLSPEC void CDECL alias(char *name, char *action);
+extern DECLSPEC char CDECL *getalias(char *name);
+extern DECLSPEC void CDECL writecfg();
+
+// console
+extern DECLSPEC void CDECL keypress(int code, bool isdown, int cooked);
+extern DECLSPEC void CDECL renderconsole();
+extern DECLSPEC void CDECL conoutf(const char *s, ...);
+extern DECLSPEC char CDECL *getcurcommand();
+extern DECLSPEC void CDECL writebinds(FILE *f);
+
+// menus
+extern DECLSPEC bool CDECL rendermenu();
+extern DECLSPEC void CDECL menuset(int menu);
+extern DECLSPEC void CDECL menumanual(int m, int n, char *text);
+extern DECLSPEC void CDECL sortmenu(int start, int num);
+extern DECLSPEC bool CDECL menukey(int code, bool isdown);
+extern DECLSPEC void CDECL newmenu(char *name);
+
+// serverbrowser
+extern DECLSPEC void CDECL addserver(char *servername);
+extern DECLSPEC char CDECL *getservername(int n);
+extern DECLSPEC void CDECL writeservercfg();
+
+// rendergl
+extern DECLSPEC void CDECL purgetextures();
+extern DECLSPEC void CDECL gl_drawframe(int w, int h, float curfps);
+extern DECLSPEC void CDECL mipstats(int a, int b, int c);
+extern DECLSPEC void CDECL vertf(float v1, float v2, float v3, sqr *ls, float t1, float t2);
+extern DECLSPEC void CDECL addstrip(int tex, int start, int n);
+extern DECLSPEC int CDECL lookuptexture(int tex, int &xs, int &ys);
+
+// rendercubes
+extern DECLSPEC void CDECL resetcubes();
+extern DECLSPEC void CDECL render_flat(int tex, int x, int y, int size, int h, sqr *l1, sqr *l2, sqr *l3, sqr *l4, bool isceil);
+extern DECLSPEC void CDECL render_flatdelta(int wtex, int x, int y, int size, float h1, float h2, float h3, float h4, sqr *l1, sqr *l2, sqr *l3, sqr *l4, bool isceil);
+extern DECLSPEC void CDECL render_square(int wtex, float floor1, float floor2, float ceil1, float ceil2, int x1, int y1, int x2, int y2, int size, sqr *l1, sqr *l2, bool topleft);
+extern DECLSPEC void CDECL render_tris(int x, int y, int size, bool topleft, sqr *h1, sqr *h2, sqr *s, sqr *t, sqr *u, sqr *v);
+extern DECLSPEC void CDECL addwaterquad(int x, int y, int size);
+extern DECLSPEC int CDECL renderwater(float hf);
+extern DECLSPEC void CDECL finishstrips();
+extern DECLSPEC void CDECL setarraypointers();
+
+// client
+extern DECLSPEC void CDECL localservertoclient(uchar *buf, int len);
+extern DECLSPEC void CDECL connects(char *servername);
+extern DECLSPEC void CDECL disconnect(int onlyclean = 0, int async = 0);
+extern DECLSPEC void CDECL toserver(char *text);
+extern DECLSPEC void CDECL addmsg(int rel, int num, int type, ...);
+extern DECLSPEC bool CDECL multiplayer();
+extern DECLSPEC bool CDECL allowedittoggle();
+extern DECLSPEC void CDECL sendpackettoserv(void *packet);
+extern DECLSPEC void CDECL gets2c();
+extern DECLSPEC void CDECL c2sinfo(dynent *d);
+extern DECLSPEC void CDECL neterr(char *s);
+extern DECLSPEC void CDECL initclientnet();
+extern DECLSPEC bool CDECL netmapstart();
+extern DECLSPEC int CDECL getclientnum();
+extern DECLSPEC void CDECL changemapserv(char *name, int mode);
+extern DECLSPEC void CDECL writeclientinfo(FILE *f);
+
+// clientgame
+extern DECLSPEC void CDECL mousemove(int dx, int dy); 
+extern DECLSPEC void CDECL updateworld(int millis);
+extern DECLSPEC void CDECL startmap(char *name);
+extern DECLSPEC void CDECL changemap(char *name);
+//extern DECLSPEC void CDECL initclient();
+extern DECLSPEC void CDECL spawnplayer(dynent *d);
+extern DECLSPEC void CDECL selfdamage(int damage, int actor, dynent *act);
+extern DECLSPEC dynent CDECL *newdynent();
+extern DECLSPEC char CDECL *getclientmap();
+extern DECLSPEC const char CDECL *modestr(int n);
+extern DECLSPEC void CDECL zapdynent(dynent *&d);
+extern DECLSPEC dynent CDECL *getclient(int cn);
+extern DECLSPEC void CDECL timeupdate(int timeremain);
+extern DECLSPEC void CDECL resetmovement(dynent *d);
+extern DECLSPEC void CDECL fixplayer1range();
+
+// clientextras
+extern DECLSPEC void CDECL renderclients();
+extern DECLSPEC void CDECL renderclient(dynent *d, bool team, char *mdlname, bool hellpig, float scale);
+extern DECLSPEC void CDECL showscores(bool on);
+extern DECLSPEC void CDECL renderscores();
+
+// world
+extern DECLSPEC void CDECL setupworld(int factor);
+extern DECLSPEC void CDECL empty_world(int factor, bool force);
+extern DECLSPEC void CDECL remip(block &b, int level = 0);
+extern DECLSPEC void CDECL remipmore(block &b, int level = 0);
+extern DECLSPEC int CDECL closestent();
+extern DECLSPEC int CDECL findentity(int type, int index = 0);
+extern DECLSPEC void CDECL trigger(int tag, int type, bool savegame);
+extern DECLSPEC void CDECL resettagareas();
+extern DECLSPEC void CDECL settagareas();
+extern DECLSPEC entity CDECL *newentity(int x, int y, int z, char *what, int v1, int v2, int v3, int v4);
+
+// worldlight
+extern DECLSPEC void CDECL calclight();
+extern DECLSPEC void CDECL dodynlight(vec &vold, vec &v, int reach, int strength, dynent *owner);
+extern DECLSPEC void CDECL cleardlights();
+extern DECLSPEC block CDECL *blockcopy(block &b);
+extern DECLSPEC void CDECL blockpaste(block &b);
+
+// worldrender
+extern DECLSPEC void CDECL render_world(float vx, float vy, float vh, int yaw, int pitch, float widef, int w, int h);
+
+// worldocull
+extern DECLSPEC void CDECL computeraytable(float vx, float vy);
+extern DECLSPEC int CDECL isoccluded(float vx, float vy, float cx, float cy, float csize);
+
+// main
+
+extern DECLSPEC void CDECL *alloc(int s);
+extern DECLSPEC void CDECL quit();
+extern DECLSPEC dynent * CDECL getplayer1();
+
+// rendertext
+extern DECLSPEC void CDECL draw_text(char *str, int left, int top, int gl_num);
+extern DECLSPEC void CDECL draw_textf(char *fstr, int left, int top, int gl_num, ...);
+extern DECLSPEC int CDECL text_width(char *str);
+
+// editing
+extern DECLSPEC void CDECL cursorupdate();
+extern DECLSPEC void CDECL toggleedit();
+extern DECLSPEC void CDECL editdrag(bool isdown);
+extern DECLSPEC void CDECL setvdeltaxy(int delta, block &sel);
+extern DECLSPEC void CDECL editequalisexy(bool isfloor, block &sel);
+extern DECLSPEC void CDECL edittypexy(int type, block &sel);
+extern DECLSPEC void CDECL edittexxy(int type, int t, block &sel);
+extern DECLSPEC void CDECL editheightxy(bool isfloor, int amount, block &sel);
+extern DECLSPEC bool CDECL noteditmode();
+extern DECLSPEC void CDECL pruneundos(int maxremain = 0);
+
+// renderextras
+extern DECLSPEC void CDECL line(int x1, int y1, float z1, int x2, int y2, float z2);
+extern DECLSPEC void CDECL box(block &b, float z1, float z2, float z3, float z4);
+extern DECLSPEC void CDECL dot(int x, int y, float z);
+extern DECLSPEC void CDECL linestyle(float width, int r, int g, int b);
+extern DECLSPEC void CDECL newsphere(vec &o, float max, int type);
+extern DECLSPEC void CDECL renderspheres(int time);
+extern DECLSPEC void CDECL gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwater);
+extern DECLSPEC void CDECL readdepth(int w, int h);
+extern DECLSPEC void CDECL blendbox(int x1, int y1, int x2, int y2, bool border);
+extern DECLSPEC void CDECL damageblend(int n);
+
+// renderparticles
+extern DECLSPEC void CDECL setorient(vec &r, vec &u);
+extern DECLSPEC void CDECL particle_splash(int type, int num, int fade, vec &p);
+extern DECLSPEC void CDECL particle_trail(int type, int fade, vec &from, vec &to);
+extern DECLSPEC void CDECL render_particles(int time);
+
+// worldio
+extern DECLSPEC void CDECL save_world(char *fname);
+extern DECLSPEC void CDECL load_world(char *mname);
+extern DECLSPEC void CDECL writemap(char *mname, int msize, uchar *mdata);
+extern DECLSPEC uchar CDECL *readmap(char *mname, int *msize);
+extern DECLSPEC void CDECL loadgamerest();
+extern DECLSPEC void CDECL incomingdemodata(uchar *buf, int len, bool extras = false);
+extern DECLSPEC void CDECL demoplaybackstep();
+extern DECLSPEC void CDECL stop();
+extern DECLSPEC void CDECL stopifrecording();
+extern DECLSPEC void CDECL demodamage(int damage, vec &o);
+extern DECLSPEC void CDECL demoblend(int damage);
+
+// physics
+extern DECLSPEC void CDECL moveplayer(dynent *pl, int moveres, bool local);
+extern DECLSPEC bool CDECL collide(dynent *d, bool spawn, float drop, float rise);
+extern DECLSPEC void CDECL entinmap(dynent *d);
+extern DECLSPEC void CDECL setentphysics(int mml, int mmr);
+extern DECLSPEC void CDECL physicsframe();
+
+// sound
+extern DECLSPEC void CDECL playsound(int n, vec *loc = 0);
+extern DECLSPEC void CDECL playsoundc(int n);
+extern DECLSPEC void CDECL updatevol();
+
+// rendermd2
+extern DECLSPEC void CDECL rendermodel(char *mdl, int frame, int range, int tex, float rad, float x, float y, float z, float yaw, float pitch, bool teammate, float scale, float speed, int snap = 0, int basetime = 0);
+extern DECLSPEC mapmodelinfo CDECL &getmminfo(int i);
+
+// server
+extern DECLSPEC void CDECL initserver(bool dedicated, int uprate, char *sdesc, char *ip, char *master, char *passwd, int maxcl);
+extern DECLSPEC void CDECL cleanupserver();
+extern DECLSPEC void CDECL localconnect();
+extern DECLSPEC void CDECL localdisconnect();
+extern DECLSPEC void CDECL localclienttoserver(struct _ENetPacket *);
+extern DECLSPEC void CDECL serverslice(int seconds, unsigned int timeout);
+extern DECLSPEC void CDECL putint(uchar *&p, int n);
+extern DECLSPEC int CDECL getint(uchar *&p);
+extern DECLSPEC void CDECL sendstring(char *t, uchar *&p);
+extern DECLSPEC void CDECL startintermission();
+extern DECLSPEC void CDECL restoreserverstate(vector<entity> &ents);
+extern DECLSPEC uchar CDECL *retrieveservers(uchar *buf, int buflen);
+extern DECLSPEC char msgsizelookup(int msg);
+extern DECLSPEC void CDECL serverms(int mode, int numplayers, int minremain, char *smapname, int seconds, bool isfull);
+extern DECLSPEC void CDECL servermsinit(const char *master, char *sdesc, bool listen);
+extern DECLSPEC void CDECL sendmaps(int n, string mapname, int mapsize, uchar *mapdata);
+extern DECLSPEC ENetPacket *recvmap(int n);
+
+// weapon
+extern DECLSPEC void CDECL selectgun(int a = -1, int b = -1, int c =-1);
+extern DECLSPEC void CDECL shoot(dynent *d, vec &to);
+extern DECLSPEC void CDECL shootv(int gun, vec &from, vec &to, dynent *d = 0, bool local = false);
+extern DECLSPEC void CDECL createrays(vec &from, vec &to);
+extern DECLSPEC void CDECL moveprojectiles(float time);
+extern DECLSPEC void CDECL projreset();
+extern DECLSPEC char CDECL *playerincrosshair();
+extern DECLSPEC int CDECL reloadtime(int gun);
+
+// monster
+extern DECLSPEC void CDECL monsterclear();
+extern DECLSPEC void CDECL restoremonsterstate();
+extern DECLSPEC void CDECL monsterthink();
+extern DECLSPEC void CDECL monsterrender();
+extern DECLSPEC dvector CDECL &getmonsters();
+extern DECLSPEC void CDECL monsterpain(dynent *m, int damage, dynent *d);
+extern DECLSPEC void CDECL endsp(bool allkilled);
+
+// entities
+extern DECLSPEC void CDECL renderents();
+extern DECLSPEC void CDECL putitems(uchar *&p);
+extern DECLSPEC void CDECL checkquad(int time);
+extern DECLSPEC void CDECL checkitems();
+extern DECLSPEC void CDECL realpickup(int n, dynent *d);
+extern DECLSPEC void CDECL renderentities();
+extern DECLSPEC void CDECL resetspawns();
+extern DECLSPEC void CDECL setspawn(uint i, bool on);
+extern DECLSPEC void CDECL teleport(int n, dynent *d);
+extern DECLSPEC void CDECL baseammo(int gun);
+
+#ifdef __cplusplus
+}
+#endif
