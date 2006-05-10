6f2a0cfc225df930bb150cc3510144426357b3d8
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-05-10 04:24:03 +0000

    Finally got it working a little bit.

--- SdlDotNet/examples/OpenGlFont/OpenGlFont.cs
+++ SdlDotNet/examples/OpenGlFont/OpenGlFont.cs
@@ -43,6 +43,11 @@ namespace SdlDotNet.Examples.OpenGlFont
 		string phrase3 = "On an OpenGl Surface ";
 		Surface screen;
 		Font font;
+		// Angle For The Triangle ( NEW )
+		float rtri;
+		// Angle For The Quad ( NEW ) 
+		float rquad;
+
 
 		#region Run Loop
 		/// <summary>
@@ -71,7 +76,8 @@ namespace SdlDotNet.Examples.OpenGlFont
 			screen = Video.SetVideoModeWindowOpenGL(this.width, this.height);
 			Events.Quit += new QuitEventHandler(this.Quit);
 			Events.Tick += new TickEventHandler(this.Tick);
-			font = new Font(filePath + dataDirectory + fontName, 20);
+			font = new Font(filePath + dataDirectory + fontName, 18);
+			Video.GLDoubleBufferEnabled = true;
 		}
 
 		[STAThread]
@@ -92,21 +98,24 @@ namespace SdlDotNet.Examples.OpenGlFont
 			Gl.glMatrixMode(Gl.GL_PROJECTION);
 			// Reset The Projection Matrix
 			Gl.glLoadIdentity();
+			Gl.glOrtho( -2.0, 2.0, -2.0, 2.0, -20.0, 20.0 );
 			// Calculate The Aspect Ratio Of The Window
-			Glu.gluPerspective(45.0F, (width / (float)height), 0.1F, 100.0F);
+			//Glu.gluPerspective(45.0F, (width / (float)height), 0.1F, 100.0F);
 			// Select The Modelview Matrix
 			Gl.glMatrixMode(Gl.GL_MODELVIEW);
 			// Reset The Modelview Matrix
 			Gl.glLoadIdentity();
-			// Enable Texture Mapping ( NEW )
-			Gl.glEnable(Gl.GL_TEXTURE_2D);
+//			// Enable Texture Mapping ( NEW )
+//			Gl.glEnable(Gl.GL_TEXTURE_2D);
 			// Enable Smooth Shading
 			Gl.glShadeModel(Gl.GL_SMOOTH);
-			// Black Background
-			Gl.glClearColor(0.0F, 0.0F, 0.0F, 0.5F);
-			// Depth Buffer Setup
-			Gl.glClearDepth(1.0F);
-			// The Type Of Depth Testing To Do
+//			// Black Background
+//			Gl.glClearColor(0.0F, 0.0F, 0.0F, 0.5F);
+//			// Depth Buffer Setup
+//			Gl.glClearDepth(1.0F);
+			// Enables Depth Testing
+			Gl.glEnable(Gl.GL_DEPTH_TEST);
+//			// The Type Of Depth Testing To Do
 			Gl.glDepthFunc(Gl.GL_LEQUAL);
 			// Really Nice Perspective Calculations
 			Gl.glHint(Gl.GL_PERSPECTIVE_CORRECTION_HINT, Gl.GL_NICEST);
@@ -114,16 +123,171 @@ namespace SdlDotNet.Examples.OpenGlFont
 
 		int i;
 		SurfaceGl surfaceGl = new SurfaceGl();
-		
+
+		#region void DrawGLScene
+
+		/// <summary>
+		/// Renders the scene
+		/// </summary>
+		protected void DrawGLScene()
+		{
+			// Clear Screen And Depth Buffer
+			//Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+			// Reset The Current Modelview Matrix
+			//Gl.glLoadIdentity();   
+//			// Move Left 1.5 Units And Into The Screen 6.0
+//			Gl.glTranslatef(-1.5f, 0, -6);   
+//			// Rotate The Triangle On The Y axis ( NEW )
+//			Gl.glRotatef(rtri, 0, 1, 0);
+//			// Drawing Using Triangles
+//			Gl.glBegin(Gl.GL_TRIANGLES);
+//			// Red
+//			Gl.glColor3f(1, 0, 0); 
+//			// Top Of Triangle (Front)
+//			Gl.glVertex3f(0, 1, 0);
+//			// Green
+//			Gl.glColor3f(0, 1, 0); 
+//			// Left Of Triangle (Front)
+//			Gl.glVertex3f(-1, -1, 1);
+//			// Blue
+//			Gl.glColor3f(0, 0, 1); 
+//			// Right Of Triangle (Front)
+//			Gl.glVertex3f(1, -1, 1);
+//			// Red
+//			Gl.glColor3f(1, 0, 0); 
+//			// Top Of Triangle (Right)
+//			Gl.glVertex3f(0, 1, 0);
+//			// Blue
+//			Gl.glColor3f(0, 0, 1); 
+//			// Left Of Triangle (Right)
+//			Gl.glVertex3f(1, -1, 1);
+//			// Green
+//			Gl.glColor3f(0, 1, 0); 
+//			// Right Of Triangle (Right)
+//			Gl.glVertex3f(1, -1, -1);
+//			// Red
+//			Gl.glColor3f(1, 0, 0); 
+//			// Top Of Triangle (Back)
+//			Gl.glVertex3f(0, 1, 0);
+//			// Green
+//			Gl.glColor3f(0, 1, 0); 
+//			// Left Of Triangle (Back)
+//			Gl.glVertex3f(1, -1, -1);
+//			// Blue
+//			Gl.glColor3f(0, 0, 1); 
+//			// Right Of Triangle (Back)
+//			Gl.glVertex3f(-1, -1, -1);   
+//			// Red
+//			Gl.glColor3f(1, 0, 0); 
+//			// Top Of Triangle (Left)
+//			Gl.glVertex3f(0, 1, 0);
+//			// Blue
+//			Gl.glColor3f(0, 0, 1); 
+//			// Left Of Triangle (Left)
+//			Gl.glVertex3f(-1, -1, -1);   
+//			// Green
+//			Gl.glColor3f(0, 1, 0); 
+//			// Right Of Triangle (Left)
+//			Gl.glVertex3f(-1, -1, 1);
+//			// Finished Drawing The Triangle
+//			Gl.glEnd();
+			// Reset The Current Modelview Matrix
+			Gl.glLoadIdentity();   
+			// Move Right 1.5 Units And Into The Screen 7.0
+			Gl.glTranslatef(1.5f, 0, -7);
+			// Rotate The Quad On The X, Y, and Z Axis ( NEW )
+			Gl.glRotatef(rquad, 1, 1, 1);
+			// Set The Color To Blue One Time Only
+			Gl.glColor3f(0.5f, 0.5f, 1);
+			// Draw A Quad
+			Gl.glBegin(Gl.GL_QUADS);   
+			// Set The Color To Green
+			Gl.glColor3f(0, 1, 0); 
+			// Top Right Of The Quad (Top)
+			Gl.glVertex3f(1, 1, -1);
+			// Top Left Of The Quad (Top)
+			Gl.glVertex3f(-1, 1, -1);
+			// Bottom Left Of The Quad (Top)
+			Gl.glVertex3f(-1, 1, 1);
+			// Bottom Right Of The Quad (Top)
+			Gl.glVertex3f(1, 1, 1);
+			// Set The Color To Orange
+			Gl.glColor3f(1, 0.5f, 0);
+			// Top Right Of The Quad (Bottom)
+			Gl.glVertex3f(1, -1, 1);
+			// Top Left Of The Quad (Bottom)
+			Gl.glVertex3f(-1, -1, 1);
+			// Bottom Left Of The Quad (Bottom)
+			Gl.glVertex3f(-1, -1, -1);   
+			// Bottom Right Of The Quad (Bottom)
+			Gl.glVertex3f(1, -1, -1);
+			// Set The Color To Red
+			Gl.glColor3f(1, 0, 0); 
+			// Top Right Of The Quad (Front)
+			Gl.glVertex3f(1, 1, 1);
+			// Top Left Of The Quad (Front)
+			Gl.glVertex3f(-1, 1, 1);
+			// Bottom Left Of The Quad (Front)
+			Gl.glVertex3f(-1, -1, 1);
+			// Bottom Right Of The Quad (Front)
+			Gl.glVertex3f(1, -1, 1);
+			// Set The Color To Yellow
+			Gl.glColor3f(1, 1, 0); 
+			// Top Right Of The Quad (Back)
+			Gl.glVertex3f(1, -1, -1);
+			// Top Left Of The Quad (Back)
+			Gl.glVertex3f(-1, -1, -1);   
+			// Bottom Left Of The Quad (Back)
+			Gl.glVertex3f(-1, 1, -1);
+			// Bottom Right Of The Quad (Back)
+			Gl.glVertex3f(1, 1, -1);
+			// Set The Color To Blue
+			Gl.glColor3f(0, 0, 1); 
+			// Top Right Of The Quad (Left)
+			Gl.glVertex3f(-1, 1, 1);
+			// Top Left Of The Quad (Left)
+			Gl.glVertex3f(-1, 1, -1);
+			// Bottom Left Of The Quad (Left)
+			Gl.glVertex3f(-1, -1, -1);   
+			// Bottom Right Of The Quad (Left)
+			Gl.glVertex3f(-1, -1, 1);
+			
+			// Set The Color To Violet
+			Gl.glColor3f(1, 0, 1); 
+			// Top Right Of The Quad (Right)
+			Gl.glVertex3f(1, 1, -1);
+			// Top Left Of The Quad (Right)
+			Gl.glVertex3f(1, 1, 1);
+			// Bottom Left Of The Quad (Right)
+			Gl.glVertex3f(1, -1, 1);
+			// Bottom Right Of The Quad (Right)
+			Gl.glVertex3f(1, -1, -1);
+			// Done Drawing The Quad
+			Gl.glEnd();
+			Gl.glMatrixMode(Gl.GL_MODELVIEW);
+			
+			// Increase The Rotation Variable For The Triangle ( NEW )
+			rtri += 0.2f;
+			// Decrease The Rotation Variable For The Quad ( NEW )
+			rquad -= 0.15f;
+		}
+
+		#endregion void DrawGLScene
+
 		private void Tick(object sender, TickEventArgs e)
 		{
+			Gl.glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
 			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+			DrawGLScene();
+			Gl.glPushMatrix();
 			surfaceGl.Surface = font.Render(phrase1 + i++, Color.White);
 			surfaceGl.Draw(new Point(0, 0));
-			surfaceGl.Surface = font.Render(phrase2 + i++, Color.White);
-			surfaceGl.Draw(new Point(100,100));
-			surfaceGl.Surface = font.Render(phrase3 + i++, Color.White);
-			surfaceGl.Draw(new Point(200, 200));
+			Gl.glPopMatrix();
+			//surfaceGl.Surface = font.Render(phrase2 + i++, Color.White);
+			//surfaceGl.Draw(new Point(100,100));
+			//surfaceGl.Surface = font.Render(phrase3 + i++, Color.White);
+			//surfaceGl.Draw(new Point(200, 200));
+			
 			Video.GLSwapBuffers();
 		}
 
--- SdlDotNet/extras/OpenGl/SurfaceGl.cs
+++ SdlDotNet/extras/OpenGl/SurfaceGl.cs
@@ -26,13 +26,15 @@ using Tao.OpenGl;
 namespace SdlDotNet.OpenGl
 {
 	/// <summary>
-	/// Summary description for FontGl.
+	/// Summary description for SurfaceGl.
 	/// </summary>
 	public class SurfaceGl
 	{
 		Surface surface;
 		Bitmap textureImage;
 		int textureID;
+		BitmapData bitmapData;
+		bool mode2D;
 
 		/// <summary>
 		/// 
@@ -57,11 +59,25 @@ namespace SdlDotNet.OpenGl
 		{
 			get
 			{
-				if (this.textureID == 0)
-				{
-					Gl.glGenTextures(1, out this.textureID);
-				}
-				return this.textureID;
+				int texture;
+				Gl.glGenTextures(1, out texture);
+				Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture);
+				// Rectangle For Locking The Bitmap In Memory
+				Rectangle rectangle = 
+					new Rectangle(0, 0, textureImage.Width, textureImage.Height);
+				this.textureImage.RotateFlip(RotateFlipType.RotateNoneFlipY); 
+
+				// Get The Bitmap's Pixel Data From The Locked Bitmap
+				bitmapData = 
+					textureImage.LockBits(rectangle, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+						
+				// Typical Texture Generation Using Data From The Bitmap
+				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, textureImage.Width, textureImage.Height, 0, Gl.GL_BGR, Gl.GL_UNSIGNED_BYTE, bitmapData.Scan0);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+				
+				//}
+				return texture;
 			}
 		}
 
@@ -81,17 +97,11 @@ namespace SdlDotNet.OpenGl
 		/// </summary>
 		public Surface ResizeSurface(Surface surface)
 		{
-			return surface.Resize(new Size(NextPowerOfTwo(this.surface.Width), NextPowerOfTwo(this.surface.Height)));
+			return surface.Resize(new Size(NextPowerOfTwo(surface.Width), NextPowerOfTwo(surface.Height)));
 		}
 
 		private static int NextPowerOfTwo(int x)
 		{
-//			int bit = 1;
-//			while ( bit < x)
-//			{
-//				bit <<=1;
-//			}
-//			return bit;
 			double logbase2 = (Math.Log(x) / Math.Log(2));
 			return (int)Math.Round(Math.Pow(2, Math.Ceiling(logbase2)));
 		}
@@ -115,9 +125,60 @@ namespace SdlDotNet.OpenGl
 			}
 			set
 			{
-				this.surface = value.Resize(new Size(NextPowerOfTwo(value.Width), NextPowerOfTwo(value.Height)));;
-				this.textureID = this.TextureID;
+				this.surface = this.ResizeSurface(value);
 				this.textureImage = this.surface.Bitmap;
+				this.textureID = this.TextureID;
+			}
+		}
+
+		private bool Mode2D
+		{
+			get
+			{
+				return this.mode2D;
+			}
+			set
+			{
+				if (value)
+				{
+					Surface screen = Video.Screen;
+
+					Gl.glPushAttrib(Gl.GL_ENABLE_BIT);
+					Gl.glDisable(Gl.GL_DEPTH_TEST);
+					Gl.glDisable(Gl.GL_CULL_FACE);
+					Gl.glEnable(Gl.GL_TEXTURE_2D);
+
+					/* This allows alpha blending of 2D textures with the scene */
+					Gl.glEnable(Gl.GL_BLEND);
+					Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+
+					Gl.glViewport(0, 0, screen.Width, screen.Height);
+
+					Gl.glMatrixMode(Gl.GL_PROJECTION);
+					Gl.glPushMatrix();
+					Gl.glLoadIdentity();
+
+					Gl.glOrtho(0.0, (double)screen.Width, (double)screen.Height, 0.0, 0.0, 1.0);
+
+					Gl.glMatrixMode(Gl.GL_MODELVIEW);
+					Gl.glPushMatrix();
+					Gl.glLoadIdentity();
+
+					Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_DECAL);
+					this.mode2D = value;
+				}
+				else
+				{
+					//Gl.glDeleteTextures(1, ref this.textureID);
+					//Gl.glDisable(Gl.GL_BLEND);	
+					//Gl.glEnable(Gl.GL_DEPTH_TEST);
+					Gl.glMatrixMode(Gl.GL_MODELVIEW);
+					Gl.glPopMatrix();
+					Gl.glMatrixMode(Gl.GL_PROJECTION);
+					Gl.glPopMatrix();
+					Gl.glPopAttrib();
+					this.mode2D = value;
+				}
 			}
 		}
 
@@ -128,55 +189,10 @@ namespace SdlDotNet.OpenGl
 		{
 			if (textureImage != null)
 			{
-				Surface screen = Video.Screen;
+				Mode2D = true;
 
-				this.textureImage.RotateFlip(RotateFlipType.RotateNoneFlipY); 
-				/* Note, there may be other things you need to change,
-				   depending on how you have your OpenGL state set up.
-				*/
-				Gl.glPushAttrib(Gl.GL_ENABLE_BIT);
-				Gl.glDisable(Gl.GL_DEPTH_TEST);
-				Gl.glDisable(Gl.GL_CULL_FACE);
-				Gl.glEnable(Gl.GL_TEXTURE_2D);
-
-				/* This allows alpha blending of 2D textures with the scene */
-				Gl.glEnable(Gl.GL_BLEND);
-				Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
-
-				Gl.glViewport(0, 0, screen.Width, screen.Height);
-
-				Gl.glMatrixMode(Gl.GL_PROJECTION);
-				Gl.glPushMatrix();
-				Gl.glLoadIdentity();
-
-				Gl.glOrtho(0.0, (double)screen.Width, (double)screen.Height, 0.0, 0.0, 1.0);
-
-				Gl.glMatrixMode(Gl.GL_MODELVIEW);
-				Gl.glPushMatrix();
-				Gl.glLoadIdentity();
-
-				Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_DECAL);
-
-				// Rectangle For Locking The Bitmap In Memory
-				Rectangle rectangle = 
-					new Rectangle(0, 0, textureImage.Width, textureImage.Height);
-
-				// Get The Bitmap's Pixel Data From The Locked Bitmap
-				BitmapData bitmapData = 
-					textureImage.LockBits(rectangle, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-						
-				// Typical Texture Generation Using Data From The Bitmap
 				Gl.glBindTexture(Gl.GL_TEXTURE_2D, this.textureID);
-				Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, textureImage.Width, textureImage.Height, 0, Gl.GL_BGR, Gl.GL_UNSIGNED_BYTE, bitmapData.Scan0);
-					
-				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
-				Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
-	
-				/* Draw a quad at location */
 				Gl.glBegin(Gl.GL_QUADS);
-				/* Recall that the origin is in the lower-left corner
-					   That is why the TexCoords specify different corners
-					   than the Vertex coors seem to. */
 				Gl.glTexCoord2f(0.0f, 1.0f); 
 				Gl.glVertex2f(location.X, location.Y);
 				Gl.glTexCoord2f(1.0f, 1.0f); 
@@ -188,23 +204,16 @@ namespace SdlDotNet.OpenGl
 				Gl.glEnd();
 	
 				/* Bad things happen if we delete the texture before it finishes */
-				Gl.glFinish();
+				//Gl.glFinish();
 
-				Gl.glDeleteTextures(1, ref this.textureID);
-				Gl.glDisable(Gl.GL_BLEND);	
-				Gl.glEnable(Gl.GL_DEPTH_TEST);
-				Gl.glMatrixMode(Gl.GL_MODELVIEW);
-				Gl.glPopMatrix();
-				Gl.glMatrixMode(Gl.GL_PROJECTION);
-				Gl.glPopMatrix();
-				Gl.glPopAttrib();
+				Mode2D = false;
 
 				if(textureImage != null) 
 				{
 					// If Texture Exists
-					textureImage.UnlockBits(bitmapData); 
+					//textureImage.UnlockBits(bitmapData); 
 					// Unlock The Pixel Data From Memory
-					textureImage.Dispose();   
+					//textureImage.Dispose();   
 					// Dispose The Bitmap
 				}
 			}
