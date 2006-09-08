54c2337691dbd41423b81a5fef03e0862aecab3a
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-09-08 18:29:26 +0000

    full support for 2.0 build. requires two slightly different prebuild.xml files

--- SdlDotNet/scripts/sharpdev2.bat
+++ SdlDotNet/scripts/sharpdev2.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-..\tools\Prebuild\Prebuild.exe /target sharpdev2 /file ..\prebuild.xml /pause
+..\tools\Prebuild\Prebuild.exe /target sharpdev2 /file ..\prebuild2.xml /pause
--- SdlDotNet/scripts/vs2005.bat
+++ SdlDotNet/scripts/vs2005.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-..\tools\Prebuild\Prebuild.exe /target vs2005 /file ..\prebuild.xml /pause
+..\tools\Prebuild\Prebuild.exe /target vs2005 /file ..\prebuild2.xml /pause
