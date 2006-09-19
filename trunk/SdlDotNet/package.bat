f81e16d784d363ce5b393c01b1161c22d6010d5a
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-09-19 16:29:24 +0000

    create .net 2.0 build files

--- trunk/SdlDotNet/package.bat
+++ trunk/SdlDotNet/package.bat
@@ -1,1 +1,2 @@
+tools\Prebuild\Prebuild.exe /target nant /file prebuild.xml
 NAnt.exe -buildfile:package.xml
---
+++ trunk/SdlDotNet/package2.bat
@@ -0,0 +1,2 @@
+tools\Prebuild\Prebuild.exe /target nant /file prebuild2.xml
+NAnt.exe -buildfile:package2.xml
