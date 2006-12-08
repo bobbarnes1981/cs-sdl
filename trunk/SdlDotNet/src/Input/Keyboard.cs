a295b316f9185ee04216c804f62702bcbb2c742b
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2006-12-08 21:37:48 +0000

    reformat

--- trunk/SdlDotNet/src/Input/Keyboard.cs
+++ trunk/SdlDotNet/src/Input/Keyboard.cs
@@ -1,6 +1,6 @@
+#region LICENSE
 /*
- * $RCSfile$
- * Copyright (C) 2004, 2005 David Hudson (jendave@yahoo.com)
+ * Copyright (C) 2004 - 2006 David Hudson (jendave@yahoo.com)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,6 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#endregion LICENSE
 
 using System;
 using System.Text;
