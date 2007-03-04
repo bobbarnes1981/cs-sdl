379cf7e414324e048fefc7b7676ef8d447ae2f9b
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2007-03-04 11:09:47 +0000

    added SharpNes emulator

---
+++ trunk/SdlDotNet/examples/SharpNes/Properties/AssemblyInfo.cs
@@ -0,0 +1,66 @@
+#region License
+/*
+Copyright (c) 2005, Jonathan Turner
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Sharpnes nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion License
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+using System.Resources;
+using System.Diagnostics.CodeAnalysis;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("SharpNES")]
+[assembly: AssemblyDescription("An 8-bit NES emulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The SDL.NET Project")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("http://cs-sdl.sourceforge.net")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("0.3.*")]
+
+// The following attributes specify the key for the sign of your assembly. See the
+// .NET Framework documentation for more information about signing.
+// This is not required, if you don't want signing let these attributes like they're.
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyName("")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.Execution)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.SkipVerification)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.UnmanagedCode)]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "SdlDotNetExamples.SharpNes", MessageId = "Nes")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "type", Target = "SdlDotNetExamples.SharpNes.NesCartridge", MessageId = "Nes")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "type", Target = "SdlDotNetExamples.SharpNes.Ppu", MessageId = "Ppu")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "member", Target = "SdlDotNetExamples.SharpNes.Ppu.RestartPpu():System.Void", MessageId = "Ppu")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "type", Target = "SdlDotNetExamples.SharpNes.NesEngine", MessageId = "Nes")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "member", Target = "SdlDotNetExamples.SharpNes.Ppu.NesPalette", MessageId = "Nes")]
+
---
+++ trunk/SdlDotNet/examples/SharpNes/SharpNesMain.cs
@@ -0,0 +1,102 @@
+#region License
+/*
+Copyright (c) 2005, Jonathan Turner
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Sharpnes nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion License
+
+// created on 2/4/2005 at 8:26 PM
+using System;
+using System.Threading;
+using SdlDotNet.Core;
+
+namespace SdlDotNetExamples.SharpNes
+{
+    public static class SharpNesMain
+    {
+        //private static Thread gameThread;
+        private static bool gameIsRunning;
+        private static NesEngine myEngine;
+        //private static ThreadStart myThreadCreator;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //If they gave us a rom name on the commandline, run it.
+            if (args.GetLength(0) == 0)
+            {
+                Run("MarioBro.nes");
+            }
+            else
+            {
+                Run(args[0]);
+            }
+        }
+
+        public static void Run(string defaultRom)
+        {
+            //our game-specific bool
+            //FIXME: move this to a more sane place when I figure out where to put it
+            gameIsRunning = false;
+            myEngine = new NesEngine();
+            //If they gave us a ROM to run on the commandline, go ahead and start it up 
+            if (!String.IsNullOrEmpty(defaultRom))
+            {
+                RunCart(defaultRom);
+            }
+        }
+
+        static void RunCart(string filename)
+        {
+            if (gameIsRunning)
+            {
+                //myEngine.QuitEngine(this, new QuitEventArgs());
+                //while (!myEngine.hasQuit);
+
+                //gameThread.Join();
+                gameIsRunning = false;
+                myEngine.RestartEngine();
+            }
+
+            if (myEngine.LoadCart(filename))
+            {
+                //myThreadCreator = new ThreadStart(myEngine.RunCart);
+                //gameThread = new Thread(myThreadCreator);
+                //gameThread.Start();
+                myEngine.RunCart();
+                gameIsRunning = true;
+            }
+            Events.Run();
+        }
+
+        //void PlayPauseActivated(object o, EventArgs e)
+        //{
+        //    myEngine.TogglePause();
+        //}
+
+        //void FullScreenActivated(object o, EventArgs e)
+        //{
+        //    myEngine.MyPpu.MyVideo.ToggleFullscreen();
+        //}
+
+        //void QuitActivated(object o, EventArgs e)
+        //{
+        //    if (!myEngine.IsQuitting)
+        //    {
+        //        myEngine.QuitEngine();
+        //        //gameThread.Join();
+        //    }
+        //}
+    }
+}
