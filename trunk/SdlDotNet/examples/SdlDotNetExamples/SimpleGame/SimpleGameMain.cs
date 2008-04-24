9eb35c1357dd02bca2ea712b65a75fe19c3def67
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2008-04-24 06:54:15 +0000

    remove process code. add an exception for Linux

--- trunk/SdlDotNet/examples/SdlDotNetExamples/SimpleGame/SimpleGameMain.cs
+++ trunk/SdlDotNet/examples/SdlDotNetExamples/SimpleGame/SimpleGameMain.cs
@@ -39,44 +39,44 @@ namespace SdlDotNetExamples.LargeDemos
         [STAThread]
         public static void Run()
         {
-            if (CheckInstance() == null)
-            {
-                EventManager eventManager = new EventManager();
-                InputController inputController =
-                    new InputController(eventManager);
-                Game game = new Game(eventManager);
-                game.Start();
-                inputController.Go();
-            }
+            //if (CheckInstance() == null)
+            //{
+            EventManager eventManager = new EventManager();
+            InputController inputController =
+                new InputController(eventManager);
+            Game game = new Game(eventManager);
+            game.Start();
+            inputController.Go();
+            //}
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public static Process CheckInstance()
-        {
-            Process current = Process.GetCurrentProcess();
-            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <returns></returns>
+        //public static Process CheckInstance()
+        //{
+        //    Process current = Process.GetCurrentProcess();
+        //    Process[] processes = Process.GetProcessesByName(current.ProcessName);
 
-            //Loop through the running processes in with the same name 
-            foreach (Process process in processes)
-            {
-                //Ignore the current process 
-                if (process.Id != current.Id)
-                {
-                    //Make sure that the process is running from the exe file. 
-                    if (Assembly.GetExecutingAssembly().Location.
-                        Replace("/", "\\") == current.MainModule.FileName)
-                    {
-                        //Return the other process instance.  
-                        return process;
-                    }
-                }
-            }
-            //No other instance was found, return null.  
-            return null;
-        }
+        //    //Loop through the running processes in with the same name 
+        //    foreach (Process process in processes)
+        //    {
+        //        //Ignore the current process 
+        //        if (process.Id != current.Id)
+        //        {
+        //            //Make sure that the process is running from the exe file. 
+        //            if (Assembly.GetExecutingAssembly().Location.
+        //                Replace("/", "\\") == current.MainModule.FileName)
+        //            {
+        //                //Return the other process instance.  
+        //                return process;
+        //            }
+        //        }
+        //    }
+        //    //No other instance was found, return null.  
+        //    return null;
+        //}
 
         /// <summary>
         /// Lesson Title
