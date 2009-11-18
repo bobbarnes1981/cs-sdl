1d854b93eef181ba87c28ef0bae7bd1014ade0e4
jendave <jendave@4b1caa98-69dc-424c-88d8-cec6a96211cd>
2009-11-18 17:39:13 +0000

    rename

--- trunk/Arcane/CrystalKeepRulings/src/arcane/deckbuilder/crystalkeeprulings/CrystalKeepRulingsPlugin.java
+++ trunk/Arcane/CrystalKeepRulings/src/arcane/deckbuilder/crystalkeeprulings/CrystalKeepRulingsPlugin.java
@@ -1,48 +1,47 @@
-
 package arcane.deckbuilder.crystalkeeprulings;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
+//import java.io.BufferedReader;
+//import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
+//import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+//import java.sql.SQLException;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Map.Entry;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+//import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
-import arcane.Arcane;
+//import arcane.Arcane;
 import arcane.ArcaneException;
-import arcane.Card;
-import arcane.CardDataStore.CardDataStoreConnection;
+//import arcane.Card;
+//import arcane.CardDataStore.CardDataStoreConnection;
 import arcane.deckbuilder.DeckBuilderPlugin;
 import arcane.deckbuilder.ui.DeckBuilder;
-import arcane.ui.ArcaneFrame;
+//import arcane.ui.ArcaneFrame;
 import arcane.ui.util.ProgressDialog;
 
-public class CrystalkeepRulingsPlugin extends DeckBuilderPlugin {
-	public void install (final DeckBuilder deckBuilder) {
+public class CrystalKeepRulingsPlugin extends DeckBuilderPlugin {
+	public void install(final DeckBuilder deckBuilder) {
 		JMenu menu = new JMenu(getName());
 		{
 			JMenuItem loadMenuItem = new JMenuItem("Update rulings...");
 			menu.add(loadMenuItem);
 			loadMenuItem.addActionListener(new ActionListener() {
-				public void actionPerformed (ActionEvent evt) {
+				public void actionPerformed(ActionEvent evt) {
 					updateRules(deckBuilder);
 				}
 			});
@@ -50,43 +49,53 @@ public class CrystalkeepRulingsPlugin extends DeckBuilderPlugin {
 		deckBuilder.addPluginMenu(menu);
 	}
 
-	public void install (ProgressDialog dialog){
+	public void install(ProgressDialog dialog) {
 
 	}
 
-	private void updateRules (DeckBuilder deckBuilder) {
-		final ProgressDialog dialog = new ProgressDialog(deckBuilder, "Crystal Keep");
+	private void updateRules(DeckBuilder deckBuilder) {
+		final ProgressDialog dialog = new ProgressDialog(deckBuilder,
+				"Crystal Keep");
 		dialog.setMessage("Downloading rulings summaries...");
 		dialog.setAlwaysOnTop(true);
 		dialog.setValue(-1);
 
 		new Thread(new Runnable() {
-			public void run () {
+			public void run() {
 				try {
-					InputStream input = new URL("http://crystalkeep.com/magic/rules/summaries/rule-txt.zip").openStream();
-					ZipInputStream zip = new ZipInputStream(new BufferedInputStream(input));
+					InputStream input = new URL(
+							"http://crystalkeep.com/magic/rules/summaries/rule-txt.zip")
+							.openStream();
+					ZipInputStream zip = new ZipInputStream(
+							new BufferedInputStream(input));
 					OutputStream out;
 					BufferedOutputStream dest = null;
 					try {
 						ZipEntry entry;
-						while((entry = zip.getNextEntry()) != null) {
-							out = new FileOutputStream("data/" + entry.getName());
+						while ((entry = zip.getNextEntry()) != null) {
+							out = new FileOutputStream("data/"
+									+ entry.getName());
 							dest = new BufferedOutputStream(out, 2048);
 							int count;
 							byte data[] = new byte[2048];
 							while ((count = zip.read(data, 0, 2048)) != -1) {
 								dest.write(data, 0, count);
-								}
+							}
 						}
 					} finally {
 						dest.flush();
 						dest.close();
 						zip.close();
 					}
-					JOptionPane.showMessageDialog(dialog, "Changes will take affect when the application is restarted.",
-							"Crystal Keep Rulings Summaries", JOptionPane.WARNING_MESSAGE);
+					JOptionPane
+							.showMessageDialog(
+									dialog,
+									"Changes will take affect when the application is restarted.",
+									"Crystal Keep Rulings Summaries",
+									JOptionPane.WARNING_MESSAGE);
 				} catch (IOException ex) {
-					throw new ArcaneException("Error downloading rulings summaries.", ex);
+					throw new ArcaneException(
+							"Error downloading rulings summaries.", ex);
 				} finally {
 					dialog.setVisible(false);
 				}
@@ -97,7 +106,7 @@ public class CrystalkeepRulingsPlugin extends DeckBuilderPlugin {
 		dialog.dispose();
 	}
 
-	public String getName () {
+	public String getName() {
 		return "Crystal Keep Rulings";
 	}
 }
